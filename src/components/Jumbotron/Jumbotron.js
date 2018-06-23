dce033fd6a53987e2b3fc5b9120ef63b8cae27ed
Brian Witte <brianw0991@gmail.com>
2018-06-22 20:01:02 -0500

    added react components

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import "./Jumbotron.css";
+
+
+const Jumbotron = () => (
+<div className="jumbotron jumbotron-fluid">
+  <div className="container text-center">
+    <h1 className="display-4">Car Clicky!</h1>
+    <p className="lead">Click on any image to earn points! Don't click the same image twice though!</p>
+  </div>
+</div>
+);
+export default Jumbotron;
